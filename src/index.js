9c7fea2399538b6d24333bd9138f51a490683858
mdquayle <misternopants@gmail.com>
2021-06-24 20:39:49 -0400

    Completed? I think

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newForm = document.querySelector('form')
+newForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input#new-task-description');
+    const tasks = document.querySelector('div#list ul');
+    const taskList = document.createElement('li')
+    tasks.appendChild(taskList);
+    taskList.innerText = input.value;
+    event.target.reset();
+  })
+})
